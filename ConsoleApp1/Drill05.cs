645d103473cfd304032518ccaac67ad6156848fb
XPGram <xp.gram@gmail.com>
2019-04-30 22:34:42 -0700

    add solution: drill 05

---
+++ ConsoleApp1/Drill05.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int age;
+            bool hadDUI;
+            int speedingTickets;
+
+            bool approved = true;
+
+            Console.WriteLine("Insurance Applicant Form");
+            Console.WriteLine();
+
+            Console.Write("What is your age?  ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Have you ever had a DUI (\"true\" or \"false\")?  ");
+            hadDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("How many speeding tickets do you have?  ");
+            speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            // Determine applicant approval
+            if (age <= 15) approved = false;
+            if (hadDUI) approved = false;
+            if (speedingTickets > 3) approved = false;
+
+            Console.WriteLine();
+
+            // Report to the applicant
+            if (approved)
+                Console.WriteLine("You were approved!");
+            else
+                Console.WriteLine("You have not been approved.");
+
+            Console.Read();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ ConsoleApp1:
 Drill 01: Testing IO  
 Drill 02: Datatypes and input storage  
 Drill 03: Practice with math operators  
-Drill 04: IO, Multiplication, Comparison  
+Drill 04: IO + Math and Comparison Operators  
+Drill 05: IO and Boolean Logic  
