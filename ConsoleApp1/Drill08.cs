6309d1c54517562a73455ebfb206458ad7f1bc22
XPGram <xp.gram@gmail.com>
2019-05-01 00:10:04 -0700

    add solution: drill 08

---
+++ ConsoleApp1/Drill08.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s1 = "Yes";
+            string s2 = "No";
+            string s3 = "I dunno";
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine(s1 + ' ' + s2 + ' ' + s3);
+            Console.WriteLine((s1 + ' ' + s3 + ' ' + s2).ToUpper());
+
+            sb.Append("I'll repeat myself. ");
+            for (int i = 0; i < 20; i++)
+            {
+                sb.Append("And I'll do it again. ");
+            }
+            sb.Append("Until I can't do it no more.");
+
+            Console.WriteLine(sb);
+            Console.Read();
+        }
+    }
+}
