ad2e964b4670192b11f8207cdb5a325c77486203
XPGram <xp.gram@gmail.com>
2019-04-30 20:29:23 -0700

    Refactor storage / add solution: drill 2

---
+++ ConsoleApp1/Drill02.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input;
+
+            string course;
+            int pageNumber;
+            bool helpNeeded;
+            string positiveExperiences;
+            string otherFeedback;
+            int hoursStudied;
+
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you on?");
+            course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            input = Console.ReadLine();
+            pageNumber = Convert.ToInt32(input);
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            input = Console.ReadLine();
+            helpNeeded = Convert.ToBoolean(input);
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            positiveExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            otherFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            input = Console.ReadLine();
+            hoursStudied = Convert.ToInt32(input);
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+        }
+    }
+}
