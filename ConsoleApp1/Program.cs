65e07e6bcb837c0b35dea78dde59d8557e174bf4
XPGram <xp.gram@gmail.com>
2019-04-30 20:30:28 -0700

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,40 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to Acme Accounting Systems");
-            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            string input;
+
+            string course;
+            int pageNumber;
+            bool helpNeeded;
+            string positiveExperiences;
+            string otherFeedback;
+            int hoursStudied;
+
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you on?");
+            course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            input = Console.ReadLine();
+            pageNumber = Convert.ToInt32(input);
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            input = Console.ReadLine();
+            helpNeeded = Convert.ToBoolean(input);
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            positiveExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            otherFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            input = Console.ReadLine();
+            hoursStudied = Convert.ToInt32(input);
+
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
             Console.Read();
         }
     }
