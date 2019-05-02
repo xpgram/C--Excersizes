89bf114204d498ba30c5a265704c5978da8c7862
XPGram <xp.gram@gmail.com>
2019-05-02 16:18:24 -0700

    add solution: drill 12

---
+++ ConsoleApp1/Drill12/MathOps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MathOps
+    {
+        public static int Double(int n)
+        {
+            return n + n;
+        }
+
+        public static int Square(int n)
+        {
+            return n * n;
+        }
+
+        public static int FirstDigit(int n)
+        {
+            return n % 10;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Drill 08: Strings and StringBuilder
 Drill 09: Arrays and Lists  
 Drill 10: Loops, Arrays and Lists  
 Drill 11: Exceptions, Try/Catch Blocks  
+Drill 12: Classes, Methods  
